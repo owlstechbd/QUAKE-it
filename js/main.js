6b8f179516e99a1c2e0c95925e6e8d6b9d04a4b9
owlstechbd <owlstech.net@gmail.com>
2025-11-28 18:42:43 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(".owl-carousel").owlCarousel({
+      loop: true,       // Do not loop
+      margin: 10,
+      nav: true,         // optional next/prev buttons
+      dots: false,
+      autoplay: false,   // Disable auto-slide
+      mouseDrag: true,   // Enable drag
+      touchDrag: true,
+      pullDrag: true,
+      responsive:{
+          0:{
+              items:1.1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3.5  // Show 3.5 items
+          }
+      }
+  });
+
+
+  
